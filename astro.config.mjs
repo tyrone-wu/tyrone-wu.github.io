c71a0b92dae3a209dbbbdc324c7f61761aa74572
tkwu <tkwu@ncsu.edu>
2023-03-08 19:51:28 -0500

    added custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tyrone-wu.github.io",
+  site: "https://www.tyrone-wu.io",
   integrations: [react()],
 });
