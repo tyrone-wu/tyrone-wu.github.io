969f3feb672940939c8bfe88cc3a0b1cb036484b
tkwu <tkwu@ncsu.edu>
2023-03-09 13:16:32 -0500

    added chakra ui; not working tho

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,17 +1,54 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { Button, Center, ChakraProvider, ColorModeScript, IconButton, useColorMode } from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import theme from "../styles/theme";
 
-const NavigationBar = () => {
-  const expanded = useState(false)
+// interface Props {
+//   expanded: boolean;
+//   toggle: () => void;
+// }
+
+export default function NavigationBar() {
+  // Toggle dark/light mode
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  // Toggle hamburger menu dropdown 
+  const [ expanded, setExpanded ] = useState(false);
+  function toggleMenu() {
+    setExpanded((prevState) => !prevState);
+  };
 
   return (
     <>
-      <span>
-        <p>
-          Nav Bar
-        </p>
-      </span>
+      <ChakraProvider theme={theme} >
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+
+        <Center>
+          <IconButton 
+            icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />} 
+            onClick={toggleColorMode}
+            aria-label="Color mode switcher"
+          >
+            Switch Mode
+          </IconButton>
+
+          {/* <HamburgerMenu expanded={expanded} toggle={toggleMenu} /> */}
+        </Center>
+      </ChakraProvider>
     </>
   );
 };
 
-export default NavigationBar
+// function HamburgerMenu({ expanded, toggle }: Props) {
+//   return (
+//     <>
+//       <Button variant="outline" colorScheme="blue" onClick={toggle}>
+//         {expanded ? 
+//           <p>true</p>
+//           :
+//           <p>false</p>
+//         }
+//       </Button>
+//     </>
+//   );
+// };
