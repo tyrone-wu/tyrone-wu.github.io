fde41e931c8a071b4bc4714d451b33f8378b756c
tkwu <tkwu@ncsu.edu>
2023-03-08 19:45:32 -0500

    added react to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://tyrone-wu.github.io"
+  site: "https://tyrone-wu.github.io",
+  integrations: [react()],
 });
---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+const NavigationBar = () => {
+  const expanded = useState(false)
+
+  return (
+    <>
+      <span>
+        <p>
+          Nav Bar
+        </p>
+      </span>
+    </>
+  );
+};
+
+export default NavigationBar
