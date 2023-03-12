da4f80c49b69669dda6d2de0ba8d9dd1f818b0e6
tkwu <tkwu@ncsu.edu>
2023-03-12 05:30:57 -0400

    switch from astro to nextjs

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-import react from "@astrojs/react";
-
-// https://astro.build/config
-export default defineConfig({
-  site: "https://tyrone-wu.github.io",
-  integrations: [react()],
-});
