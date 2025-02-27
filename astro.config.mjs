4f6f17bda558432d09979203dad6e3f6dbe58581
Tyrone Wu <wudevelops@gmail.com>
2025-02-27 23:10:46 +0000

    switching to cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tyrone-wu.github.io",
+  site: "https://tyrone-wu.pages.dev",
   trailingSlash: "ignore",
   prefetch: true,
   vite: {
